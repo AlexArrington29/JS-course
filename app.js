3b70168724b1bf0649c2b58757feaa828acc7582
Alex <alexarrington64@gmail.com>
2024-01-05 17:07:55 -0500

    just working on some more JS

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log(alex)
+let cash = 20
+let price = 50 
+let difference = cash - price
+
+if (cash > price) {
+    console.log(`you paid extra - here's ${difference} dollars change`)
+}
+
+else if (cash === price) {
+    console.log("you paid the exact amount, have a nice day!")
+}
+
+else {
+    console.log(`not enough money - you still owe ${difference * -1} dollars`)
+}

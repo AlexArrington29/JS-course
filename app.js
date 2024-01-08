7dacc5a9fa0620f562ba298b54f56782e8f8c343
Alex <alexarrington64@gmail.com>
2024-01-07 23:17:21 -0500

    working on functions in JS

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-for (let i = 1; i <= 20; ++i) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log(`${i} -> Frontend Simplified`);
-  } 
-  else if (i % 3 === 0) {
-    console.log(`${i} -> Frontend`);
-  } 
-  else if (i % 5 === 0) {
-    console.log(`${i} ->Simplified`);
-  } 
-  else {
-    console.log(`${i} -> ${i}`);
-  }
+function welcomePersonToFES (firstName, lastName) {
+    console.log(`Welcome to FES, ${firstName} ${lastName}`)
 }
+
+welcomePersonToFES("Alex", "Arrington")
